46aa3b9821b4d5fc13fef9ebdacb75e81bc8db26
frohlichramon01 <frohlichramon01@gmail.com>
2023-06-28 11:06:24 -0300

    segundo dia

--- crud.ts
+++ crud.ts
@@ -1,13 +1,96 @@
 import fs from "fs"; //ES6
+import {v4 as uuid} from 'uuid';
 
 // const fs = require("fs");
 const DB_FILE_PATH = "db"
 
 console.log("[CRUD]");
 
-function create(content:string){
-    fs.writeFileSync(DB_FILE_PATH, content)
-    return content;
+type UUID = string;
+
+interface Todo{
+    id: UUID,
+    date: string;
+    content: string;
+    done: boolean;
 }
 
-create("Hoje eu vou fazer X222 com:crud");
+function create(content:string): Todo{
+    const todo: Todo = {
+        id: uuid(),
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    };
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ]
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({todos,},null, 2),)
+    return todo;
+}
+
+function read(): Array<Todo>{
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if(!db.todos){
+        return [];
+    }
+    return db.todos;
+}
+
+function update(id:UUID, partialTodo:Partial<Todo>): Todo{
+    let updatedTodo;
+    const todos = read();
+
+    todos.forEach((currentTodo) => {
+        const isToUpdate = currentTodo.id === id;
+        if(isToUpdate){
+            updatedTodo = Object.assign(currentTodo, partialTodo);
+        }
+    });
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
+
+    if(!updatedTodo){
+        throw new Error("Please, provide another ID!");
+    }
+    return updatedTodo;
+}
+
+function updateContentById(id:UUID, content:string): Todo{
+    return update(id, {content,})
+}
+
+function deleteById(id:UUID){
+    const todos = read();
+
+    const todosWithoutOne = todos.filter((todo)=> {
+        if(id === todo.id){
+            return false;
+            }
+        return true;
+    });
+    
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos: todosWithoutOne,
+    }, null, 2));
+}
+
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "")
+}
+
+CLEAR_DB();
+create("Primeira TODO");
+create("2 TODO");
+create("3 TODO");
+create("4 TODO");
+
+
+console.log(read());
